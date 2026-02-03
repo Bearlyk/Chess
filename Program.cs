81f018e8d46fbdd31abd3b81d9d8d07e3eec8444
bearlyk <tonyjhorel@gmail.com>
2026-02-03 19:00:50 -0400

    firs part

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿using System.IO;
+
+write();
+
+static void write()
+{
+    string ruta = @"c:\my_chess_program\table3.txt";
+    string myname = "Anthony Jhorel Inca Figueroa";
+    File.WriteAllText(ruta, myname);
+}
