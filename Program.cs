ba457e938388ca9e26b329ec9f8d09769a26aa79
bearlyk <tonyjhorel@gmail.com>
2026-02-03 20:32:16 -0400

    I'm missing the looped part to read

--- Program.cs
+++ Program.cs
@@ -1,10 +1,23 @@
 ﻿using System.IO;
+ leer();
+//write();
 
-write();
+ static void write()
 
-static void write()
 {
     string ruta = @"c:\my_chess_program\table3.txt";
     string myname = "Anthony Jhorel Inca Figueroa";
     File.WriteAllText(ruta, myname);
+}
+
+static void leer()
+{
+ string contenido = File.ReadAllText("table.txt");
+
+
+Console.WriteLine(contenido);
+
+
+Console.ReadKey(); 
+   
 }
