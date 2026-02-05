11343c8537c347caa3a10210ab64515e47e2579a
bearlyk <tonyjhorel@gmail.com>
2026-02-04 20:14:14 -0400

    I couldn't place the reader and more errors started appearing, and now I have to fix everything from the beginning.

--- Program.cs
+++ Program.cs
@@ -1,23 +1,8 @@
 ﻿using System.IO;
- leer();
-//write();
-
- static void write()
-
+write();
+static void write()
 {
     string ruta = @"c:\my_chess_program\table3.txt";
     string myname = "Anthony Jhorel Inca Figueroa";
     File.WriteAllText(ruta, myname);
-}
-
-static void leer()
-{
- string contenido = File.ReadAllText("table.txt");
-
-
-Console.WriteLine(contenido);
-
-
-Console.ReadKey(); 
-   
 }
