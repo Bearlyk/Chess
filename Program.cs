14698900f65b96311013986300676a8c65a8d77a
bearlyk <tonyjhorel@gmail.com>
2026-02-05 20:43:56 -0400

    Problem solved and finished

--- Program.cs
+++ Program.cs
@@ -1,8 +1,43 @@
 ﻿using System.IO;
+
 write();
+leer();
+
 static void write()
 {
     string ruta = @"c:\my_chess_program\table3.txt";
     string myname = "Anthony Jhorel Inca Figueroa";
     File.WriteAllText(ruta, myname);
+}
+static void leer()
+{
+   
+    string rutaArchivo = @"C:\my_chess_program\table.txt";
+
+    string contenidoUltimaVez = "";
+
+    Console.WriteLine("--- Esperando cambios en el archivo ---");
+
+    while (true)
+    {
+        if (File.Exists(rutaArchivo))
+        {
+            string contenidoActual = File.ReadAllText(rutaArchivo);
+
+            if (contenidoActual != contenidoUltimaVez)
+            {
+                Console.Clear();
+                Console.WriteLine("¡El archivo se actualizó!");
+                Console.WriteLine("--------------------------");
+                Console.WriteLine(contenidoActual);
+
+                contenidoUltimaVez = contenidoActual;
+            }
+        }
+        else
+        {
+            Console.WriteLine("No encuentro el archivo 'datos.txt'...");
+        }
+        Thread.Sleep(1000);
+    }
 }
